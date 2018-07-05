68e513e5152c448a5f9f00c5a31808589d846900
Unknown <alexlapwood@gmail.com>
2018-07-05 21:06:02 +1200

    Remove service worker (GitLab pages don't prevent file caching)

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App waitForImages={true} />, document.getElementById(
   "root"
 ) as HTMLElement);
-registerServiceWorker();
 
 const updatePageSize = () => {
   document.body.style.height =
