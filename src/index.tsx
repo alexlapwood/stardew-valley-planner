b2b83a8e58a29ebaee5fdfa4ad639e66e7071d4d
Unknown <alexlapwood@gmail.com>
2018-09-24 17:00:07 +1200

    Remove type casting for ReactDom.render

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App waitForImages={true} />, document.getElementById(
-  "root"
-) as HTMLElement);
+ReactDOM.render(<App waitForImages={true} />, document.getElementById("root"));
 
 const updatePageSize = () => {
   document.body.style.height =
